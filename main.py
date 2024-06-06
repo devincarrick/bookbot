bcea0fe10efd78eab51e18601f37a61c6c5f6f15
devin carrick <devin.carrick@gmail.com>
2024-06-06 11:20:16 -0400

    Change main.py to read and print the contents of "frankenstein.txt"

---
+++ main.py
@@ -0,0 +1,17 @@
+# Change main.py so that instead of printing "hello world", it reads the contents of the "frankenstein.txt" and prints it all to the console.
+
+def main():
+    # path to the book
+    book_path = "books/frankenstein.txt"
+    # get the text from the book
+    text = get_text(book_path)
+    print(text)
+
+# helper function to get the text from a file
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+    
+# Call the main function
+if __name__ == "__main__":
+    main()
